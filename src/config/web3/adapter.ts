0a0faada6feb7987ce548a71c85adc8eeadb28ed
Cristian F. Taborda <tabordacristianfernando@gmail.com>
2026-04-18 19:01:54 -0300

    wallet

--- src/config/web3/adapter.ts
+++ src/config/web3/adapter.ts
@@ -1,6 +1,6 @@
-import { createStorage }  from 'wagmi'
-import { WagmiAdapter }   from '@reown/appkit-adapter-wagmi'
-
+import { createStorage }                               from 'wagmi'
+import { injected, metaMask, coinbaseWallet, safe }    from 'wagmi/connectors'
+import { WagmiAdapter }                                from '@reown/appkit-adapter-wagmi'
 import { PROJECT_ID, WAGMI_CHAINS, ACTIVE_TRANSPORTS } from './constants'
 
 export const wagmiAdapter = new WagmiAdapter({
@@ -9,6 +9,26 @@ export const wagmiAdapter = new WagmiAdapter({
   transports: ACTIVE_TRANSPORTS,
   storage:    createStorage({ storage: window.localStorage }),
   ssr:        false,
+
+  multiInjectedProviderDiscovery: true,
+
+  connectors: [
+    injected(),
+    metaMask({
+      dapp: {
+        name:    'Ethernal Foundation',
+        url:     'https://ethernal.fund',
+        iconUrl: 'https://ethernal.fund/icon-512.png',
+      },
+    }),
+
+    coinbaseWallet({
+      appName: 'Ethernal Foundation',
+      appLogoUrl: 'https://ethernal.fund/icon-512.png',
+    }),
+
+    safe(),
+  ],
 })
 
 export const wagmiConfig = wagmiAdapter.wagmiConfig
